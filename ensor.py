de7c5a3af6f9740ff32b72b5e05d47d7e3c012ea
Ojas_singh <singhojas007@gmail.com>
2019-05-27 22:01:32 +0530

    added laplacian matrix

--- ensor.py
+++ ensor.py
@@ -46,11 +46,11 @@ def main():
             o = len(pdbdata[0])
             m = np.zeros((o,o))
             for i in range (0,o):
-                for j in range (0,o):
+                for j in range (0,i):
                     x1=pdbdata[1][i]
                     y1=pdbdata[2][i]
                     z1=pdbdata[3][i]
-                    x2=pdbdata[1][j]
+                    x2=pdbdata[1][j] 
                     y2=pdbdata[2][j]
                     z2=pdbdata[3][j]
                     d=((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)**(0.5)
@@ -59,24 +59,33 @@ def main():
                     
                     if np.subtract(bd[0][0],bd[0][1]) <= d <= np.add(bd[0][0],bd[0][1]):
                         m[i][j]=1
+                        m[j][i]=1
                     if np.subtract(bd[1][0],bd[1][1]) <= d <= np.add(bd[1][0],bd[1][1]):
                         m[i][j]=2
+                        m[j][i]=2
                     if np.subtract(bd[2][0],bd[2][1]) <= d <= np.add(bd[2][0],bd[2][1]):
                         m[i][j]=3
+                        m[j][i]=3
                     if d==0:
-                        m[i][j]=0            
-        Con_matrix= TemporaryFile()
-        np.save('results/Con_matrix.npy',m)
-        # np.savetxt('Con_matrix.txt',m)      
-        print m
-        print 'Connectivity Matrix saved as Con_matrix.npy under results directory.'
+                        m[i][j]=0 
+                        m[j][i]=0
+            deg = np.zeros((o,o))
+            for i in range (0,o):
+                x=0
+                for j in range (0,o):
+                    if m[i][j] != 0:
+                        x=x+1
+                deg[i][i] = x
+        Lap = deg - m
+        
+                  
+        # Con_matrix= TemporaryFile()
+        # np.save('results/Con_matrix.npy',m)
+        
+        
         
             
 
 
 if __name__ == '__main__':
    main()
-
-
-
-
