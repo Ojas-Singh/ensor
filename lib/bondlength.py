b1a8105f2e3d6aeb26bca20e6aac9ebae2b7a1d1
Ojas_singh <ms16084@iisermohali.ac.in>
2019-12-27 03:58:08 +0530

    Update bondlength.py

--- lib/bondlength.py
+++ lib/bondlength.py
@@ -6,27 +6,52 @@
 #############################################################################################################################
 
 def search(name):
-    l=[[1,2],[0,0],[0,0]]
-    # if name=='CH' or name=='HC':
-    #     l=[[1.10,.05],[0,0],[0,0]]
-    # if name=='CC' or name=='CC':
-    #     l=[[1.54,.05],[1.34,.05],[1.20,.05]]
-    # if name=='CN' or name=='NC':
-    #     l=[[1.43,.08],[1.28,.05],[1.14,.05]]
-    # if name=='CO' or name=='OC':
-    #     l=[[1.43,.05],[1.20,.05],[1.10,.05]]
-    # if name=='CS' or name=='SC':
-    #     l=[[1.8,.08],[1.68,.05],[1.13,.05]]      
-    # if name=='NO' or name=='NO':
-    #     l=[[1.43,.05],[1.23,.05],[1.13,.05]]  
-    # if name=='NN' or name=='NN':
-    #     l=[[1.45,.05],[1.23,.05],[1.1,.05]]
-    # if name=='OO' or name=='OO':
-    #     l=[[1.45,.05],[1.21,.05],[1.21,.05]]
-    # if name=='SO' or name=='OS':
-    #     l=[[1.45,.05],[1.23,.05],[1.23,.05]]
-    # if name=='SN' or name=='NS':
-    #     l=[[1.45,.05],[1.23,.05],[1.23,.05]]
+    l=[0,2,100]
+    if name=='CH' or name=='HC':
+        l[0]=1.2
+    if name=='CC' or name=='CC':
+        l[0]=1.7
+    if name=='CN' or name=='NC':
+        l[0]=1.6
+    if name=='CO' or name=='OC':
+        l[0]=1.5
+    if name=='CS' or name=='SC':
+        l[0]=1.8  
+    if name=='NO' or name=='ON':
+        l[0]=1.36  
+    if name=='NN' or name=='NN':
+        l[0]=1.5
+    if name=='OO' or name=='OO':
+        l[0]=1.7
+    if name=='SO' or name=='OS':
+        l[0]=1.5
+    if name=='SN' or name=='NS':
+        l[0]=1.8
+    if name=='NN' or name=='NN':
+        l[0]=1.5
+    if name=='OO' or name=='OO':
+        l[0]=1.7
+    if name=='SS' or name=='SS':
+        l[0]=1.7 
+    if name=='PP' or name=='PP':
+        l[0]=2
+    if name=='NH' or name=='HN':
+        l[0]=1.2 
+    if name=='CCL' or name=='CLC':
+        l[0]=1.8
+    if name=='SO' or name=='OS':
+        l[0]=1.5
+    if name=='HO' or name=='OH':
+        l[0]=1.2
+    if name=='PO' or name=='OP':
+        l[0]=1.65 
+    if name=='CP' or name=='PC':
+        l[0]=1.7   
+    if name=='NP' or name=='PN':
+        l[0]=1.7 
+    if name=='PP' or name=='PP':
+        l[0]=2
+       
     return l
 
 
