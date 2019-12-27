d89f9daaf4b755d43a374adc825b792bc7765ebd
Ojas_singh <ms16084@iisermohali.ac.in>
2019-12-27 05:51:23 +0530

    Update ensor.py

--- ensor.py
+++ ensor.py
@@ -1,9 +1,15 @@
 import sys
+import os
+import glob
+
 import networkx as nx
-import numpy as np
+import numpy as np 
+from lib import fragerrr as fgr
 from lib import fragrr as fg
 from lib import pdb2con as chef
 from lib import plotter as plotter
+from lib import xyzexport as xyz
+
 
 print ""
 print "  ______ _   _  _____  ____  _____   "
@@ -33,7 +39,8 @@ def main():
         N=pdbdata[0]
         E=pdbdata[4]
         Coord=[pdbdata[1],pdbdata[2],pdbdata[3]]
-        Adj_Matrix=np.load('results/Adj_matrix.npy')
+        Adj_Matrix=np.load('results/Con_matrix.npy')
+        # Adj_Matrix=np.load('results/Adj_matrix.npy')
         Mol=[Adj_Matrix,N]
         x=[]
         
@@ -47,11 +54,17 @@ def main():
         Parts=frag[0]
         for i in range (0,len(frag[0])):
             print "Nodes and edges in part",i+1,"is:",fg.nodes_edges(frag[0][i])
-        print "No. of bond broken :",len(frag[1])
+        print "No. of bond broken :",len(frag[1])/2l
+        print "No. of non-bond broken :",len(frag[2])/2
         plotter.plot(G,frag,E,N,Coord,filename)
-    
-       
-        
+
+        xyz.export(pdbdata,Mol,Parts)
+
+
+
+files = glob.glob('results/*')
+for f in files:
+    os.remove(f)
         
         
             
