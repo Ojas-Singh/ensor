34f708c2ad8fee3740f044aa4600084509f9a516
Ojas_singh <ms16084@iisermohali.ac.in>
2020-01-25 09:56:20 +0530

    Add files via upload

---
+++ lib/congugate.py
@@ -0,0 +1,43 @@
+import networkx 
+from networkx.algorithms.components.connected import connected_components
+
+def to_graph(l):
+    G = networkx.Graph()
+    for part in l:
+        # each sublist is a bunch of nodes
+        G.add_nodes_from(part)
+        # it also imlies a number of edges:
+        G.add_edges_from(to_edges(part))
+    return G
+
+def to_edges(l):
+    """ 
+        treat `l` as a Graph and returns it's edges 
+        to_edges(['a','b','c','d']) -> [(a,b), (b,c),(c,d)]
+    """
+    it = iter(l)
+    last = next(it)
+
+    for current in it:
+        yield last, current
+        last = current    
+
+def system(mol_Matrix):
+    G=networkx.Graph(mol_Matrix)
+    rings= list(networkx.cycle_basis(G))
+    d=[]
+    l=[]
+    for i in range(len(mol_Matrix[0])):
+        for j in range(0,i):
+            if mol_Matrix[i][j]==2 :
+                d.append([i+1,j+1])
+    l=l+d
+    for i in range(len(rings)):
+        a=[]
+        for j in rings[i]:
+            a.append(j+1)
+        l.append(a)
+    X = to_graph(l)
+    return connected_components(X)
+
+
